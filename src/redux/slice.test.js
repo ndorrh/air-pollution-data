421239368df4bc5234abe2f89af78967db3632a2
ndorrh <ndorrh@gmail.com>
2022-11-23 09:23:06 +0100

    Update unit test for getPollutionData function

--- src/redux/slice.test.js
+++ src/redux/slice.test.js
@@ -1,7 +1,7 @@
 import { getPollutionData } from './slice';
 
 describe('getPollutionData tests', () => {
-  it('Expect ID of second country to be 2', () => {
+  it('Expect ID of second country not to be 2', () => {
     expect(getPollutionData(2).payload).not.toBe(!2);
   });
 });
